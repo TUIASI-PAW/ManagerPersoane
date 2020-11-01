5d30037eb963c67512868aac808ffb24f6f57b0c
Adrian Alexandrescu <aalexandrescu@tuiasi.ro>
2020-11-01 16:58:16 +0200

    Realizarea legăturii dintre / și modulul persoane

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'persoane',
+    pathMatch: 'full'
+  },
+  {
+    path: 'persoane',
+    loadChildren: () => import('./persoane/persoane.module').then(mod => mod.PersoaneModule)
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
