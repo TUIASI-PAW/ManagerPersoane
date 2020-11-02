1271cd41dbcc9cfacc59937bdfbdc84650a8797e
Adrian Alexandrescu <aalexandrescu@tuiasi.ro>
2020-11-02 05:52:49 +0200

    Adăugare referință în app.module.ts pentru modulul HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
+    // nu trebuie de importat PersoaneModule; totul se întâmplă când accesăm /gestionare-persoane - lazy loading
   ],
   providers: [],
   bootstrap: [AppComponent]
