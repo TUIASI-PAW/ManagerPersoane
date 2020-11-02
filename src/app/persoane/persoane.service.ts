44eb4295926e5e8cde416479ea10318553f7049d
Adrian Alexandrescu <aalexandrescu@tuiasi.ro>
2020-11-02 05:35:42 +0200

    Accesarea în cadrul serviciului a variabilei de mediu care reprezintă adresa url a serverului extern API

--- src/app/persoane/persoane.service.ts
+++ src/app/persoane/persoane.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Persoana } from './models'; // se uită în index.ts din directorul models și o să ia /persoana.model
 
+import { environment } from '../../environments/environment'; // accesăm variabile de mediu
+
 @Injectable({
   providedIn: 'root'
 })
 export class PersoaneService {
+  private url = `${environment.apiUrl}persoane`;
 
   constructor() { }
 }
