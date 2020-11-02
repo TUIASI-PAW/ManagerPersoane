77fc5107336ed0a51ab8dc1e7623f264c327e7ba
Adrian Alexandrescu <aalexandrescu@tuiasi.ro>
2020-11-02 05:32:15 +0200

    Referirea clasei model Persoana ]n persoana.service.ts

--- src/app/persoane/persoane.service.ts
+++ src/app/persoane/persoane.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Persoana } from './models'; // se uită în index.ts din directorul models și o să ia /persoana.model
 
 @Injectable({
   providedIn: 'root'
