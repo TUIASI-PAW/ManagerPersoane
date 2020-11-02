2e456179fb30510a7439549aaae8d0a664855e33
Adrian Alexandrescu <aalexandrescu@tuiasi.ro>
2020-11-02 05:39:25 +0200

    Adăugarea logicii de interacțiune cu serverul extern API

--- src/app/persoane/persoane.service.ts
+++ src/app/persoane/persoane.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Persoana } from './models'; // se uită în index.ts din directorul models și o să ia /persoana.model
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+import { Persoana } from './models'; // se uită în index.ts din directorul models și o să ia /persoana.model
 import { environment } from '../../environments/environment'; // accesăm variabile de mediu
 
 @Injectable({
@@ -9,5 +11,17 @@ import { environment } from '../../environments/environment'; // accesăm variab
 export class PersoaneService {
   private url = `${environment.apiUrl}persoane`;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  get(): Observable<Persoana[]> {
+    return this.httpClient.get<Persoana[]>(this.url);
+  }
+
+  save(persoana: Persoana): Observable<any> {
+    return this.httpClient.post(this.url, persoana);
+  }
+
+  delete(id: number): Observable<any> {
+    return this.httpClient.delete(`${this.url}/${id}`);
+  }
 }
